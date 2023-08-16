40b2949d57b1121ee05107b52a701fe6b07f354f
ritesh007 <rithu.ritesh@gmail.com>
2023-08-15 22:40:53 -0500

    skleton code

---
+++ Task management/Task_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_managementApp.swift
+//  Task management
+//
+//  Created by rkuchuku on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Task_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
